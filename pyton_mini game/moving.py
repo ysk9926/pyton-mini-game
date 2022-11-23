14c662a8d08df5be18aa606d181252c69e968c68
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2022-11-23 23:32:01 +0900

    22.11.22__study

--- pyton_mini game/moving.py
+++ pyton_mini game/moving.py
@@ -6,26 +6,26 @@ class ControlAnimation:
         window = Tk()
         window.title("Mini Game")
 
-        self.width = 250
+        self.width = 450
         self.canvas = Canvas(window, bg = "white",
-        width = self.width, height = 50)
+        width = self.width, height = 200)
         self.canvas.pack()
 
         frame = Frame(window)
         frame.pack()
-        btResume = Button(frame, text ="Ball Race",
-        command = click1)
-        btResume.pack(side = LEFT)
-        btFaster = Button(frame, text = "PingPing",
-        command = click2)
-        btFaster.pack(side = LEFT)
-        btSlower = Button(frame, text = "Snake",
-        command = click3)
-        btSlower.pack(side=LEFT)
+        btResume = Button(frame, text ="Ball Race",fg ="white",font =("나눔고딕", 15, "bold"), width=10, height=2, bg="#225C73",
+        command = self.click1)
+        btResume.pack(side = LEFT, padx=10, pady=4)
+        btFaster = Button(frame, text = "PingPing",fg ="white",font =("나눔고딕", 15, "bold"), width=10, height=2, bg="#4B8CA6",
+        command = self.click2)
+        btFaster.pack(side = LEFT, padx=10, pady=4)
+        btSlower = Button(frame, text = "Snake",fg ="white", font =("나눔고딕", 15, "bold"), width=10, height=2, bg="#73B1BF",
+        command = self.click3)
+        btSlower.pack(side=LEFT, padx=10, pady=4)
 
         self.x = 0
-        self.sleepTime = 100
-        self.canvas.create_text(self.x,30,text = "Mini Game", tags = "text")
+        self.sleepTime = 10
+        self.canvas.create_text(self.x,100,text = "Mini Game",font =("나눔고딕", 50, "bold"),tags = "text")
 
         self.dx = 3
         self.isStopped = False
@@ -33,11 +33,11 @@ class ControlAnimation:
 
         window.mainloop()
         
-    def click1():
+    def click1(self):
         subprocess.run(["python", "race.py"])
-    def click2():
+    def click2(self):
         subprocess.run(["python", "pingpong.py"])
-    def click3():
+    def click3(self):
         subprocess.run(["python", "snake.py"])
 
 
@@ -51,6 +51,6 @@ class ControlAnimation:
             else:
                 self.x = 0
                 self.canvas.delete("text")
-                self.canvas.create_text(self.x,30,text = "Message moving?", tags = "Text")
+                self.canvas.create_text(self.x,100,text = "Mini Game",font =("나눔고딕", 50, "bold"), tags = "text")
 
 ControlAnimation()
